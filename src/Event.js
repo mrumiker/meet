f46803f612fe16c56c919fdd0961631dd21563b5
mrumiker <69427808+mrumiker@users.noreply.github.com>
2021-01-13 14:38:33 -0600

    prevent Button default behavior

--- src/Event.js
+++ src/Event.js
@@ -16,8 +16,8 @@ class Event extends Component {
     const { selected } = this.state;
     const { event } = this.props;
     const eventDetails = selected ? <div className="event-details">{event.description}</div> : '';
-    const detailsButton = selected ? <button className="details-button" onClick={this.handleButtonClicked}>Collapse</button> :
-      <button className="details-button" onClick={this.handleButtonClicked}>Details</button>;
+    const detailsButton = selected ? <button className="details-button" type="button" onClick={this.handleButtonClicked}>Collapse</button> :
+      <button className="details-button" type="button" onClick={this.handleButtonClicked}>Details</button>;
 
     return (
       <div className="event-info">
