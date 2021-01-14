aedf7384da3d3f4416cefb19bfa24650943678b7
mrumiker <69427808+mrumiker@users.noreply.github.com>
2021-01-13 23:47:47 -0600

    delete Unnecessary import

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import NumberOfEvents from '../NumberOfEvents';
-import EventList from '../EventList';
 
 describe('<NumberOfEvents /> component', () => {
   let NumberOfEventsWrapper;
