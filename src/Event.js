9e363ccbfca7e516fef76d24e629ec6c46241186
mrumiker <69427808+mrumiker@users.noreply.github.com>
2021-01-21 22:48:55 -0600

    add Styling

--- src/Event.js
+++ src/Event.js
@@ -16,12 +16,17 @@ class Event extends Component {
     const { selected } = this.state;
     const { event } = this.props;
     const eventDetails = selected ? <div className="event-details">{event.description}</div> : '';
-    const detailsButton = selected ? <button className="details-button" type="button" onClick={this.handleButtonClicked}>Collapse</button> :
-      <button className="details-button" type="button" onClick={this.handleButtonClicked}>Details</button>;
+    const detailsButton = selected ? <button className="details-btn" type="button" onClick={this.handleButtonClicked}>Collapse</button> :
+      <button className="details-btn" type="button" onClick={this.handleButtonClicked}>Details</button>;
 
     return (
       <div className="event">
-        <p className="event-title">{event.summary}</p>
+        <h1 className="event-title name">{event.summary}</h1>
+        <br />
+        <h2 className="group-name">{event.location}</h2>
+        <br />
+        <p>{event.start.dateTime}  {event.start.timeZone}</p>
+        <br />
         {eventDetails}
         <br />
         {detailsButton}
